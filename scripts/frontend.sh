152e978565e86d439b6225c9dc7554cb5eeb2873
SamvegShah013 <samvegworkspace@gmail.com>
2024-12-18 10:08:04 +0530

    Update frontend.sh

---
+++ scripts/frontend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update System and Install Required Packages
+sudo yum update -y
+sudo yum install httpd -y
+sudo yum install git -y 
+sudo yum install -y gcc-c++ make 
+sudp curl -sL https://rpm.nodesource.com/setup_21.x | sudo -E bash - 
+sudo yum install -y nodejs
+# Clone Frontend Repository
+sudo git clone https://github.com/SamvegShah013/TodoApp.git
+
+# Navigate to Frontend Folder
+cd TodoApp/frontend
+
+# Install Node.js Dependencies and Build Project
+sudo npm install
+sudo npm run build
+
+# Deploy Built Files to Apache Server
+sudo cp -r dist/* /var/www/html/
+
+# Start and Enable Apache Service
+sudo systemctl start httpd
+sudo systemctl enable httpd
