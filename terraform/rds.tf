39a54e1721384d23f8453d501376fe5736f1e5a8
SamvegShah013 <samvegworkspace@gmail.com>
2024-12-18 11:32:45 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,9 +1,9 @@
 resource "aws_db_instance" "three-tier-db" {
   allocated_storage   = 20
-  storage_type        = "gp3"
+  storage_type        = "gp2"
   engine              = "mysql"
   engine_version      = "8.0.32"  # Use a different version
-  instance_class      = "db.t3.micro"  # Use a supported instance type
+  instance_class      = "db.t4g.micro"  # Use a supported instance type
   identifier          = "three-tier-db"
   username            = "admin"
   password            = "samveg1234"
